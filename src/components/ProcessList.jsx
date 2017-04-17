93843ed01eb82c4e307ba3f68167e8af5c89aeac
John Ley <ley.johnt@gmail.com>
2017-04-17 13:59:23 -0700

    BUMPED: react, react-dom to @latest REMOVED: react-addons-test-utils, react-addons-pure-render-mixin

--- src/components/ProcessList.jsx
+++ src/components/ProcessList.jsx
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import ProcessListItem from './ProcessListItem';
 
 export default class ProcessList extends React.Component {
